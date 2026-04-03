e4dec7aac97fb34d97f9cf0dc91594a1cd5b3fd7
vstaff <vstaff22b@gmail.com>
2026-04-03 21:52:54 +1000

    feat: Новые поля фильтров

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -15,17 +15,64 @@ export const Filter: FC<FilterProps> = (props) => {
     const formData = new FormData(event.currentTarget);
 
     // создаем словарь со значениями полей формы
-    const filterField: Pick<Building, "title" | "buildType"> = {
+    const filterField: Pick<Building, "title" | "buildType" | "country" | "city"> & {
+      yearFrom: number;
+      yearTo: number;
+      heightFrom: number;
+      heightTo: number;
+    } = {
       title: formData.get("title")!.toString().toLowerCase(),
-      buildType: formData.get("buildType")!.toString().toLowerCase()
+      buildType: formData.get("buildType")!.toString().toLowerCase(),
+      country: formData.get("country")!.toString().toLowerCase(),
+      city: formData.get("city")!.toString().toLowerCase(),
+
+      yearFrom: formData.get("year-from") ? +formData.get("year-from")! : 0,
+      yearTo: formData.get("year-to") ? +formData.get("year-to")! : 0,
+      heightFrom: formData.get("height-from") ? +formData.get("height-from")! : 0,
+      heightTo: formData.get("height-to") ? +formData.get("height-to")! : 0
     };
+
+    console.log({ filterField });
 
     //фильтруем данные по значениям всех полей формы
     let arr: Building[] = props.fullData;
     for (const key in filterField) {
-      if (key !== "buildType" && key !== "title") continue;
+      if (
+        key !== "title" && 
+        key !== "buildType" && 
+        key !== "country" && 
+        key !== "city" && 
+        key !== "yearFrom" && 
+        key !== "yearTo" && 
+        key !== "heightFrom" && 
+        key !== "heightTo"
+      ) continue;
 
-      arr = arr.filter(item => item[key].toLowerCase().includes(filterField[key]));  
+      arr = arr.filter(item => {
+        if (key === "buildType" || key === "title" || key === "country" || key === "city") {
+          const stringCheckResult = item[key].toLowerCase().includes(filterField[key]);
+          
+          if (stringCheckResult) {
+            console.log(item[key], 'fits key', key)
+          }
+
+          return stringCheckResult;
+        }
+
+        console.log('number key', key)
+        const year = item.year;
+        const height = item.height;
+        console.log({year, height}, key, filterField[key])
+
+        if (!filterField[key]) return true;
+
+        if (key === "yearFrom") return year >= filterField[key];
+        if (key === "yearTo") return year <= filterField[key];
+        if (key === "heightFrom") return height >= filterField[key];
+        if (key === "heightTo") return height <= filterField[key];
+
+        console.log('no number key found');
+      });  
     }
 
     props.setDataTable(arr);
@@ -41,12 +88,42 @@ export const Filter: FC<FilterProps> = (props) => {
   return (
     <form onSubmit={handleSubmit}>
       <p>
-        <label>Название:</label>
-        <input name="title" type="text" />
+        <label htmlFor="title">Название:</label>
+        <input name="title" id="title" type="text" />
       </p>
       <p>
-        <label>Type:</label>
-        <input name="buildType" type="text" />
+        <label htmlFor="buildType">Type:</label>
+        <input name="buildType" id="buildType" type="text" />
+      </p>
+
+      <p>
+        <label htmlFor="country">Страна</label>
+        <input type="text" name="country" id="country" />
+      </p>
+
+      <p>
+        <label htmlFor="city">Город</label>
+        <input type="text" name="city" id="city" />
+      </p>
+
+      <p>
+        <label htmlFor="year-from">Год от</label>
+        <input type="number" name="year-from" id="year-from" min={1000} />
+      </p>
+
+      <p>
+        <label htmlFor="year-to">Год до</label>
+        <input type="number" name="year-to" id="year-to" min={1000} />
+      </p>
+
+      <p>
+        <label htmlFor="height-from">Высота от</label>
+        <input type="number" name="height-from" id="height-from" min={1} />
+      </p>
+
+      <p>
+        <label htmlFor="height-to">Высота до</label>
+        <input type="number" name="height-to" id="height-to" min={1} />
       </p>
       <p>
         <button type="submit">Фильтровать</button>
